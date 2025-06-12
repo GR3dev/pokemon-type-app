4477a340d5f7cc86586745e28aba756480db883b
Gil Rodriguez <gilr.dev@gmail.com>
2025-06-11 23:01:16 -0600

    Fix vscode errors for typescript in page.tsx

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,14 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
-<<<<<<< HEAD
 module.exports = {
   output: 'export',
   // or if older Next.js version
   // exportTrailingSlash: true,
   // basePath: '/your-repo-name', (if using basePath for github pages)
-}
+};
 
-
-=======
->>>>>>> 07ab0ed8cd1f3cf464297c64219254716c7c902f
 export default nextConfig;
